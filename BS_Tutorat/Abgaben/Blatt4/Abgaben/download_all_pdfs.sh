d6ed541b32ecdd3e28476d63d93268504f47b5b0
Jürgen Mattheis <matthejue@gmail.com>
2022-11-19 16:37:19 +0100

    None

--- BS_Tutorat/Abgaben/Blatt4/Abgaben/download_all_pdfs.sh
+++ BS_Tutorat/Abgaben/Blatt4/Abgaben/download_all_pdfs.sh
@@ -6,12 +6,15 @@ ID="1bWcTfzfSltE0SRiDDOx"
 ANZAHL_GRUPPEN=16
 
 # https://stackoverflow.com/questions/18460123/how-to-add-leading-zeros-for-for-loop-in-shell
-for i in $(seq -w 01 $ANZAHL_GRUPPEN); do
-  wget "https://ira.informatik.uni-freiburg.de/cgi-bin/teaching/bs-ws2223/tutordownload.cgi/${TUTORAT}${i}${BLATT}.pdf?file=${TUTORAT}${i}${BLATT}&sessionid=${ID}&bgruppe=${TUTORAT}" -O ${i}.pdf
+for gruppe in $(seq -w 01 $ANZAHL_GRUPPEN); do
+  # https://unix.stackexchange.com/questions/560651/leading-zero-fill-of-integer-variable
+  # https://unix.stackexchange.com/questions/338000/bash-assign-output-of-pipe-to-a-variable
+  # sheet_name=$(printf '%02d%d%02d' ${TUTORAT} ${gruppe} ${BLATT})
+  wget "https://ira.informatik.uni-freiburg.de/cgi-bin/teaching/bs-ws2223/tutordownload.cgi/${TUTORAT}${gruppe}${BLATT}.pdf?file=${TUTORAT}${gruppe}${BLATT}&sessionid=${ID}&bgruppe=${TUTORAT}" -O ${TUTORAT}${gruppe}${BLATT}.pdf
   # https://stackoverflow.com/questions/16152583/tell-if-a-file-is-pdf-in-bash
-  type="$(file -b ${i}.pdf)"
+  type="$(file -b ${gruppe}.pdf)"
   if [ ! "${type%%,*}" == "PDF document" ]; then
-    rm ${i}.pdf
+    rm ${gruppe}.pdf
   fi
 
   wget "https://ira.informatik.uni-freiburg.de/cgi-bin/teaching/bs-ws2223/tutordownload.cgi/loes-${BLATT}.pdf?file=loes-${BLATT}&sessionid=${ID}&bgruppe=${TUTORAT}" -O "loes-${BLATT}.pdf"
